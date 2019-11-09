2e7b38ca8a8b549b8bd0b7d39da3d32fd0a69341
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-09 14:09:05 +0100

    Adding lesson 06 - Variables

---
+++ aula 06/main.dart
@@ -0,0 +1,18 @@
+
+void main() {
+  String nome = "Fernando";
+  String sobrenome = "Moraes";
+
+  print("Meu nome é " + nome + " " + sobrenome + ".");
+
+  int idade = 25;
+  print("Minha idade é ${idade} anos.");
+
+  double altura = 1.72;
+  print("Minha altura é ${altura} metros.");
+
+  bool masculino = true;
+
+  if(masculino) print("Seu sexo é masculino.");
+  else print("Seu sexo é feminio.");
+}
