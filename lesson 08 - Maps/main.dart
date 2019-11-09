ada8a41a7cd72fe9ac995a41be0753e9d1ed5f42
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-09 14:24:26 +0100

    Adding Lesson 08 - Maps

---
+++ lesson 08 - Maps/main.dart
@@ -0,0 +1,16 @@
+
+void main() {
+  Map usuario = {
+    "nome": "Fernando",
+    "idade": 25,
+    "altura": 1.72,
+    "developer": true,
+  };
+
+  print(usuario);
+  print(usuario.length);
+  print(usuario.keys);
+  print(usuario.values);
+  print(usuario["nome"]);
+  print(usuario["idade"]);
+}
